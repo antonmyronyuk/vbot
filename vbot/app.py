387d676b7bfd7afc394c1a58e42f1f4f2cff8a46
antonmyronyuk <antonmyronyuk@gmail.com>
2022-03-07 13:37:26 +0200

    Close bot session

--- vbot/app.py
+++ vbot/app.py
@@ -3,7 +3,6 @@ import os
 
 from aiohttp import web
 
-from vbot import signals
 from vbot import views
 from vbot.bots import telegram
 from vbot.bots import viber
@@ -11,14 +10,18 @@ from vbot.bots import viber
 logging.basicConfig(level=logging.INFO)
 
 
-def create_app() -> web.Application:
-    app = web.Application()
-    # app.on_startup.append(signals.start_ping)
+def setup_routing(app: web.Application) -> None:
     app.router.add_route('GET', '/', views.health)
     app.router.add_route('GET', '/health', views.health)
     app.router.add_route('GET', '/info', views.info)
     app.router.add_route('POST', '/bots/telegram', telegram.WebhookView),
     app.router.add_route('POST', '/bots/viber', viber.webhook),
+
+
+def create_app() -> web.Application:
+    app = web.Application()
+    app.on_cleanup.append(telegram.close_bot_session)
+    setup_routing(app)
     return app
 
 
